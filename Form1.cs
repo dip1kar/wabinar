ed422dfeb0ac015bfb6c4308ba3642177eab6e52
dip1kar <39810731+dip1kar@users.noreply.github.com>
2018-11-03 13:02:34 +0530

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Barcode2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string UserName = "Rupshikha";
+            string PassWord = "12345";
+            if (UserName == textBox1.Text && PassWord == textBox2.Text)
+
+            {
+
+               // MessageBox.Show("Login successfully....!");
+                Form2 frm = new Form2();
+                frm.Show();
+
+            }
+
+            else
+            {
+                MessageBox.Show("Please enter vaild UserName");
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MessagingToolkit.Barcode;
+
+namespace Barcode2
+{
+    public partial class Form2 : Form
+
+ {
+        string filename;
+        List<MyPicture> list;
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        BarcodeEncoder Generator;
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Generator = new BarcodeEncoder();
+            Generator.IncludeLabel = true;
+            Generator.CustomLabel = textBox5.Text;
+            if (textBox5.Text != "")
+                pictureBox1.Image = new Bitmap(Generator.Encode(BarcodeFormat.Code39, textBox5.Text));
+
+
+        }
+
+
+        byte[] ConvertImageToBinary(Image img)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                img.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+                return ms.ToArray();
+            }
+        }
+
+        private async void button3_Click(object sender, EventArgs e)
+        {
+            using (picEntities db = new picEntities())
+            {
+                MyPicture pic = new MyPicture() { FileName = filename, Data = ConvertImageToBinary(pictureBox1.Image) };
+
+                db.MyPictures.Add(pic);
+                await db.SaveChangesAsync();
+                MessageBox.Show("you have been successfully saved", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    
+            }
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Generator = new BarcodeEncoder();
+            Generator.IncludeLabel = true;
+            Generator.CustomLabel = textBox5.Text;
+            Generator.CustomLabel = textBox4.Text;
+            if (textBox5.Text != "" )
+                pictureBox1.Image = new Bitmap(Generator.Encode(BarcodeFormat.Code39, textBox5.Text ));
+                
+
+
+
+        }
+    }
+}
